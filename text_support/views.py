efe40c6a3c66c941c79b6de0fd12dc554441d774
mattjmcnaughton <mattjmcnaughton@gmail.com>
2016-06-29 17:11:56 -0400

    Add testing infrastructure and refactor
    This commit adds the infrastructure we will use for testing, and writes
    a few simple tests of basic routes to check its working properly.
    
    Additionally, we move all of the routes over to `views.py` using
    `flask.Blueprint`, in order to split the code into smaller components.
    The written tests reflect this change.
    
    Signed-off-by: mattjmcnaughton <mattjmcnaughton@gmail.com>

--- text_support/views.py
+++ text_support/views.py
@@ -1,3 +1,20 @@
 """
 `views.py` is where we define the routes of this application.
 """
+
+# pylint: disable=import-error, invalid-name
+
+from flask import Blueprint
+
+# We define `app_views` as a Blueprint so that we can import the views in
+# `app.py`. This allows all of the `view` logic to be stored in a single file.
+# Read more [here](http://flask.pocoo.org/docs/0.11/blueprints/).
+app_views = Blueprint('app_views', __name__)
+
+@app_views.route("/")
+def index():
+    """
+    A simple function to render "Hello World" and status code 200 when the `/`
+    endpoint is accessed. Just for testing purposes.
+    """
+    return "Hello World!"
