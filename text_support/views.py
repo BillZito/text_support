efdf52400884692656aa941ecb2c65a88bf461af
mattjmcnaughton <mattjmcnaughton@gmail.com>
2016-06-30 20:19:16 -0400

    Render index.html
    Use the `render_template` method to render `index.html` when a request
    is made. Additionally, create the `layout.html` page which all other
    pages will extend.
    
    Signed-off-by: mattjmcnaughton <mattjmcnaughton@gmail.com>

--- text_support/views.py
+++ text_support/views.py
@@ -4,7 +4,7 @@
 
 # pylint: disable=import-error, invalid-name
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # We define `app_views` as a Blueprint so that we can import the views in
 # `app.py`. This allows all of the `view` logic to be stored in a single file.
@@ -14,7 +14,9 @@ app_views = Blueprint('app_views', __name__)
 @app_views.route("/")
 def index():
     """
-    A simple function to render "Hello World" and status code 200 when the `/`
-    endpoint is accessed. Just for testing purposes.
+    Render `templates/index.html` with a status code of 200.
+
+    Returns:
+        object: The rendered template.
     """
-    return "Hello World!"
+    return render_template("index.html")
